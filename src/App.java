fcc9eff8faf11b8b811931d32d1020444d677264
Henry <28hzisow@gannacademy.org>
2025-09-17 19:10:15 -0400

    Done

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 /**
  *  Write code that reads a series of test scores (as integers) from the user until the user 
  *  enters -1. The test scores should be in a range from 0 to 100. If the user enters an invalid number
@@ -5,11 +6,34 @@
  *  that a failing grade is below a 60. When the user enters '-1', print out the number of passing and failing students.
  */
 
-
- import java.util.*;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        int pass = 0;
+        int fail = 0;
+        System.out.println("Input your grades as integers:");
+        while (true) {
+            int score = Integer.valueOf(scanner.nextLine());
+            if (score == -1) {
+                break;
+            } else {
+                if (score <= 0 || score >= 100) {
+                    System.out.println("Invalid number. Please input a different value:");
+                } else {
+                    if (score < 60) {
+                        fail++;
+                    } else {
+                        if (score >= 60) {
+                            pass++;
+
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("You have " + pass + " passing grades.");
+        System.out.println("You have " + fail + " failing grades.");
     }
 }
